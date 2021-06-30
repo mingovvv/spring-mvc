1ba1a1411378cb76c301a63b7b7786eea42d0d81
puregyu <puregyu@naver.com>
2021-06-30 23:54:49 +0900

    init commit

---
+++ src/main/java/com/mvc/devyu/DevyuApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.devyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvc/devyu/DevyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.devyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
