8189baeb2ca35bb5a578ecf58ed4fdb2d159e8d2
puregyu <puregyu@naver.com>
2021-07-15 01:20:18 +0900

    response spec

---
+++ src/main/java/com/mvc/devyu/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,75 @@
+package com.mvc.devyu.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // status
+        response.setStatus(HttpServletResponse.SC_FOUND);
+
+        // response header
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Devyu-Header", "test");
+
+        content(response);
+        cookie(response);
+        redirect(response);
+
+        response.getWriter().write("안녕?");
+
+    }
+
+    /**
+     * content 편의 메서드
+     *
+     * @param response
+     */
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    /**
+     * cookie 편의 메서드
+     *
+     * @param response
+     */
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("devyu-cookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    /**
+     * redirect 편의 메서드
+     *
+     * @param response
+     * @throws IOException
+     */
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+
+}
