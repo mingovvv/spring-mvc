d7a70f303d3d581b06e172f51e3219ac353e5258
puregyu <puregyu@naver.com>
2021-08-22 22:22:29 +0900

    bean validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 만원이 넘어야 합니다.")
+// 제약이 많고 기능이 부족하기 때문에 실제 권장되지 않는 방식.
 public class Item {
 
     private Long id;
