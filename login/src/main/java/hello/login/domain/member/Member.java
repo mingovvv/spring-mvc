7b77e384cd1a59ddecd7d8a3f1a5aaa3a0c2145d
puregyu <puregyu@naver.com>
2021-08-26 23:01:47 +0900

    로그인 init

---
+++ login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,21 @@
+package hello.login.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+
+}
