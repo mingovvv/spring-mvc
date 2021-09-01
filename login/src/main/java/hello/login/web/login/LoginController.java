78ecb57cc780e74c655f7407dd70a30803cfb1fa
puregyu <puregyu@naver.com>
2021-09-02 00:51:15 +0900

    exception init

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -103,10 +103,12 @@ public class LoginController {
         HttpSession session = request.getSession();
         // request.getSession(true), default : 세션이 있으면 세션 반환 / 없으면 신규 생성
         // request.getSession(false) : 세션이 있으면 세션 반환 / 없으면 null 반환
-        // ** 사용자의 request 정보를 가지고 key를 생성. 해당 *key*를 클라이언트를 위해 쿠키에 담아 응답까지 하는 역할
+        // ** 사용자의 request 정보를 가지고 key를 생성. 해당 *key*를 클라이언트를 위해 쿠키에 담아 응답까지 하는 역할 (av82hsafsa-3dsad-234sf)
+        // 사용자의 request 정보를 가지고 키를 생성합니다. (이미 존재할 경우 패스). 보통 이 키는 중복되지 않음을 보장합니다. 그래서 세션에 담은 데이터가 다른 유저와 섞이는 경우는 없습니다. 그리고 해당 키를 클라이언트의 쿠키에 담아서 응답합니다.  이후 클라이언트는 모든 request에 쿠키를 전달하므로, 서버는 이 쿠키에 세션 키를 찾아서 세션 데이터가 존재할때의 처리를 할 수 있습니다.
 
-        // ** 세션에 로그인 회원 정보를 보관 : 세션에 저장하는 것 : 생성한 key에 value를 매핑하는 것 : "login-member" 는 단순 상수값이며 일종의 폴더이름 같은 것(로그인 맴버를 위한 상수값이구나 !)
         session.setAttribute("login-member", loginMember);
+        // ** 세션에 로그인 회원 정보를 보관 : 세션에 저장하는 것 : 생성한 key에 value를 매핑하는 것 : "login-member" 는 단순 상수값이며 일종의 폴더이름 같은 것(로그인 맴버를 위한 상수값이구나 !)
+        // 세션에 저장할 정보를 담는 코드 입니다. 세션 내부에서 또다시 키:값으로 데이터를 매핑하여 보관합니다. (키와 값을 Map에 저장하는것과 마찬가지 입니다.
 
         return "redirect:/";
 
