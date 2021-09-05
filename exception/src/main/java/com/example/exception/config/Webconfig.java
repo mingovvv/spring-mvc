110b5cf070dcec3466a266120fc07d1547f695c9
puregyu <puregyu@naver.com>
2021-09-05 23:40:12 +0900

    api exception

--- exception/src/main/java/com/example/exception/config/Webconfig.java
+++ exception/src/main/java/com/example/exception/config/Webconfig.java
@@ -1,15 +1,18 @@
 package com.example.exception.config;
 
 import com.example.exception.filter.LogFilter;
+import com.example.exception.resolver.MyHandlerExceptionResolver;
 import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class Webconfig implements WebMvcConfigurer {
@@ -23,14 +26,19 @@ public class Webconfig implements WebMvcConfigurer {
                 );
     }
 
-//    @Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new LogFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST); // default
-        return filterRegistrationBean;
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
     }
+
+    //    @Bean
+//    public FilterRegistrationBean logFilter() {
+//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
+//        filterRegistrationBean.setFilter(new LogFilter());
+//        filterRegistrationBean.setOrder(1);
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST); // default
+//        return filterRegistrationBean;
+//    }
 
 }
