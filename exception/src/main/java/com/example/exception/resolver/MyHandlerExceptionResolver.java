94e042f20cd951f80028fccd354c024c698636f9
puregyu <puregyu@naver.com>
2021-09-12 22:48:43 +0900

    exception resolver

--- exception/src/main/java/com/example/exception/resolver/MyHandlerExceptionResolver.java
+++ exception/src/main/java/com/example/exception/resolver/MyHandlerExceptionResolver.java
@@ -18,7 +18,8 @@ public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
             log.info("IllegalArgumentException resolver to 400");
             try {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
-                return new ModelAndView();
+                return new ModelAndView(); // 400으로 예외처리를 하기 위해 새로운 ModelAndView 객체를 생성 (exception을 sendError로 바꿔치기)
+                // new ModelAndView() : 뷰를 렌더링하지 않고 정상 흐름으로 서블릿이 리턴된다.
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -26,6 +27,18 @@ public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
         }
 
         return null;
+        // null : 널을 반환하면 다음 Exception Resolver를 찾아서 실행한다. 만약 처리할 수 있는 ExceptionResolver가 없으면 예외처리가 안되고 기존에 발생한 서블릿 밖으로 던진다.
+
+        /*
+         - ExceptionResolver 활용
+        1. 예외 상태 코드 변환
+         > 예외를 response.sendError(xxx) 호출로 변경해서 서블릿에서 상태 코드에 따른 오류를 처리하도록 위임
+        이후 WAS는 서블릿 오류 페이지를 찾아서 내부 호출, 예를 들어서 스프링 부트가 기본으로 설정한 / error 가 호출됨
+        2. 뷰 템플릿 처리
+         > ModelAndView 에 값을 채워서 예외에 따른 새로운 오류 화면 뷰 렌더링 해서 고객에게 제공
+        3. API 응답 처리
+         > response.getWriter().println("hello"); 처럼 HTTP 응답 바디에 직접 데이터를 넣어주는 것도 가능하다. 여기에 JSON 으로 응답하면 API 응답 처리를 할 수 있다.
+        */
     }
 
 }
